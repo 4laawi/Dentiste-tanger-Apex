4d72dc6260f0c52a51e2679129e6eb72a51250e9
Ali Taghi <ali.taghmar@gmail.com>
2026-05-09 01:02:08 +0100

    rgrgr

---
+++ app/dentiste-reda-saoui/page.tsx
@@ -0,0 +1,27 @@
+import RedaSaouiView from "@/components/RedaSaouiView";
+import { TRANSLATIONS } from "@/constants";
+import ClientPageLayout from "@/components/ClientPageLayout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: TRANSLATIONS.fr.seo.doctor.title,
+  description: TRANSLATIONS.fr.seo.doctor.description,
+  alternates: {
+    canonical: '/dentiste-reda-saoui',
+    languages: {
+      'en': '/en/dentiste-reda-saoui',
+      'fr': '/dentiste-reda-saoui',
+    },
+  },
+};
+
+export default function Page() {
+  const lang = 'fr';
+  const t = TRANSLATIONS[lang];
+
+  return (
+    <ClientPageLayout lang={lang} t={t} currentView="about">
+      <RedaSaouiView t={t} lang={lang} />
+    </ClientPageLayout>
+  );
+}
