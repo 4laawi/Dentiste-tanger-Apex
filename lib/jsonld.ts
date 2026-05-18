a51fc60eef5aa6e4f499d9b9d7d084ed9c4002f7
Ali Taghi <ali.taghmar@gmail.com>
2026-05-18 22:37:10 +0100

    rgrgr

--- lib/jsonld.ts
+++ lib/jsonld.ts
@@ -53,7 +53,7 @@ export function getHomeJsonLd(lang: 'en' | 'fr') {
         "priceRange": "$$",
         "currenciesAccepted": "MAD, EUR",
         "paymentAccepted": "Cash, Credit Card, Insurance",
-        "hasMap": "https://www.google.com/maps/place/APEX+Dental+Clinic/@35.7767,-5.8039,17z/",
+        "hasMap": "https://maps.google.com/maps?cid=0xd0c7fb2933d913f:0x80899102823ebcf3",
         "address": address,
         "geo": geo,
         "openingHoursSpecification": openingHours,
@@ -72,7 +72,7 @@ export function getHomeJsonLd(lang: 'en' | 'fr') {
         ],
         "sameAs": [
           "https://www.instagram.com/dr_reda_saoui/",
-          "https://www.google.com/maps/place/APEX+Dental+Clinic/@35.7767,-5.8039,17z/"
+          "https://maps.google.com/maps?cid=0xd0c7fb2933d913f:0x80899102823ebcf3"
         ],
         "aggregateRating": {
           "@type": "AggregateRating",
